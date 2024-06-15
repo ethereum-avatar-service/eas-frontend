b8686e7fa2913bc5821064bfd5af6359f6b12a22
Warm Beer <mickvd99@gmail.com>
2024-06-15 13:40:31 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,9 +8,13 @@
       </div>
     </div>
     <div class="px-4 md:px-9 py-3 w-full text-neutral-400 border-t border-neutral-100">
-      <div class="flex flex-col md:flex-row justify-between gap-6 text-sm">
+      <div class="flex flex-col lg:flex-row justify-between gap-6 text-sm">
         <span>Donate: 0xB0753614730408d91Ccc46c1f4210efF0bee648B</span>
-        <a href="https://github.com/ethereum-avatar-service" target="_blank">GitHub</a>
+        <div class="flex flex-col lg:flex-row gap-6">
+          <a href="mailto:mick@dutchbits.nl" target="_blank">Become a sponsor</a>
+          <a href="mailto:mick@dutchbits.nl" target="_blank">Contact</a>
+          <a href="https://github.com/ethereum-avatar-service" target="_blank">GitHub</a>
+        </div>
       </div>
     </div>
   </footer>
